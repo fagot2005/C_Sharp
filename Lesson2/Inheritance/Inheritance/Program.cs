c1d08a19d06fbf9963822f44eef419939197e2db
fagot2005@gmail.com <v.lashchenko.1976@gmail.com>
2021-03-26 22:33:31 +0200

    Наследование

---
+++ Lesson2/Inheritance/Inheritance/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pupil ivan = new Pupil();
+            Pupil stepan = new Pupil();
+            Pupil petro = new Pupil();
+            Pupil dmitro = new Pupil();
+
+        }
+    }
+
+    class Pupil
+    {
+
+        public virtual void Study()
+        {
+        }
+
+        public virtual void Read()
+        {
+        }
+
+        public virtual void Write()
+        {
+        }
+
+        public virtual void Relax()
+        {
+        }
+    }
+
+    class ExcelentPupil : Pupil
+    {
+        public override void Study()
+        {
+
+        }
+
+        public override void Read()
+        {
+
+        }
+
+        public override void Write()
+        {
+            
+        }
+
+        public override void Relax()
+        {
+            
+        }
+    }
+    class GoodPupil : Pupil
+    {
+    }
+    class BadPupil : Pupil
+    {
+    }
+}
