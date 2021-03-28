af78f9c0c498b7c226de920d7009481c6de4c374
fagot2005@gmail.com <v.lashchenko.1976@gmail.com>
2021-03-28 14:54:57 +0300

    Vehicle 3

--- Lesson2/Inheritance/Vehicle/Program.cs
+++ Lesson2/Inheritance/Vehicle/Program.cs
@@ -4,15 +4,15 @@ using System.Collections.Generic;
 namespace Vehicle
 {
 
-//    Задание 3 
-//Используя Visual Studio, создайте проект по шаблону Console Application.
-//Требуется:  
-//Создать класс Vehicle.
-//В теле класса создайте поля:  координаты и параметры средств передвижения (цена, скорость, год выпуска).  
-//Создайте 3 производных класса Plane, Саг и Ship.
-//Для класса Plane должна быть определена высота и количество пассажиров.  
-//Для класса Ship — количество пассажиров и порт приписки.
-//Написать программу, которая выводит на экран информацию о каждом средстве передвижения.
+    //    Задание 3 
+    //Используя Visual Studio, создайте проект по шаблону Console Application.
+    //Требуется:  
+    //Создать класс Vehicle.
+    //В теле класса создайте поля:  координаты и параметры средств передвижения (цена, скорость, год выпуска).  
+    //Создайте 3 производных класса Plane, Саг и Ship.
+    //Для класса Plane должна быть определена высота и количество пассажиров.  
+    //Для класса Ship — количество пассажиров и порт приписки.
+    //Написать программу, которая выводит на экран информацию о каждом средстве передвижения.
 
 
     class Program
@@ -20,18 +20,18 @@ namespace Vehicle
         static void Main(string[] args)
         {
             List<Vehicle> list = new List<Vehicle>();
-            //Console.WriteLine("Hi");
             Vehicle boing737 = new Plane("34434", 23.3, 4);
             Vehicle porshe = new Car("842342", 200, 2);
             list.Add(boing737);
             list.Add(porshe);
-            //boing737.Сoordinates = ;
-            //Console.WriteLine(boing737.Сoordinates);
-            foreach(Vehicle element in list)
+            foreach (Vehicle element in list)
             {
-                //if(element.GetType = Vehicle.P)
-                Plane plane = (Plane)element;
-                Console.WriteLine(plane.Сoordinates, plane.Height, plane.Count);
+                Plane plane = element as Plane;
+                if (plane != null)
+                    Console.WriteLine(plane.Сoordinates, plane.Height, plane.Count);
+                Car car = element as Car;
+                if (car != null)
+                    Console.WriteLine(car.Сoordinates, car.Power, car.CountDoors);
             }
             Console.ReadKey();
         }
@@ -41,22 +41,22 @@ namespace Vehicle
     {
         public string Сoordinates { get; set; }
         //public Vehicle Vehicle { get; set; }
-        
-     }
+
+    }
 
     class Plane : Vehicle
     {
         public double Height { get; set; }
         public int Count { get; set; }
 
-        public Plane( string coordinate, double height, int count)
+        public Plane(string coordinate, double height, int count)
         {
             this.Сoordinates = coordinate;
             this.Height = height;
             this.Count = count;
         }
     }
-    
+
     class Car : Vehicle
     {
         public int Power { get; set; }
